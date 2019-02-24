4dc271d96f4603cd532eb13d07b2d977e9ca3d3e
SungHwanHwang <springhwan5435@gmail.com>
2019-02-24 23:39:48 +0900

    20190224_nasa-apod

--- nasa-apod/src/App.js
+++ nasa-apod/src/App.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
-import ViewerTemplate from './components/viewerTemplate';
+import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/spaceNavigator';
 import Viewer from './components/viewer';
 
 import * as api from './lib/api';
 
-//   Test For Modified
-
 class App extends Component {
 
+  //  render() 위는 초기 state 자리
+  state = {
+    loading: false,
+    maxDate: null,
+    date: null,
+    url: null,
+    mediaType: null
+  }
+
+  //  async/await
   getAPOD = async (date) => {
+    
+    //  loading중이라면 무시
+    if(this.state.loading) return;
+
+    this.setState({
+      loading: true
+    });
+
     try {
+      //  console.log(response);
       const response = await api.getAPOD(date);
-      console.log(response);
-    } catch (e) {
-      // 오류가 났을 경우
+      
+      //  response.date를 비구조화 할당
+      //  새로운 이름 지어주기
+      //  아래 형식에서만 ':' (비구조화할당이 진행되는 와중이라면)
+      const { date: retrievedDate, url, media_type: mediaType } = response.data;
+
+      if(!this.state.maxDate) {
+        //  만약 maxDate 값이 존재하지 않으면 지급 받은 date로 지정
+        this.setState({
+          maxDate: retrievedDate
+        })
+      }
+
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      });
+
+    } catch(e) {
       console.log(e);
     }
+
+    //  로딩 상태 종료
+    this.setState({
+      loading: false
+    });
   }
 
   componentDidMount() {
@@ -24,6 +63,10 @@ class App extends Component {
   }
 
   render() {
+
+    //  const 변수
+    const { url, mediaType, loading } = this.state;
+
     return (
       <ViewerTemplate  
         spaceNavigator={<SpaceNavigator/>}
@@ -33,11 +76,17 @@ class App extends Component {
           mediaType="image"
           />)}
           */
-        viewer={(<Viewer 
+         /*
+         viewer={(<Viewer 
           url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
           mediaType="video"
           />)}
-
+     */
+          viewer = {(<Viewer
+            url={url}
+            mediaType={mediaType}
+            loading={loading} />
+          )}
           />
     );
   }
--- nasa-apod/src/lib/api.js
+++ nasa-apod/src/lib/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 //  axios 사용법
 //  https://github.com/axios/axios
 export function getAPOD(date = '') {
-    return axios.get('https://api.nasa.gov/planetary/apod?api_key=W9Gh55nWWBsEGiOLkxunhkrWBQ6XJkAPAAkXdi9J&date=${date}');
+    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=gUUPvWchwgA7H4Fx0c7UOqXQndND6qhS9aYi1Aos&date=${date}`);
 }
